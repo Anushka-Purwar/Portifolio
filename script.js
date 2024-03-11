5d6c39fa36c0cf0b11dabc07b1b4c0db3374571a
Anushka Purwar <anushkapurwar799@gmail.com>
2024-03-12 00:45:56 +0530

    updating

--- script.js
+++ script.js
@@ -122,4 +122,24 @@ function toggleMenu() {
   };
   
   const animation = new AnimateOnScroll(options);
+
+
+
+//   var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("skills-details");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length} ;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
   
